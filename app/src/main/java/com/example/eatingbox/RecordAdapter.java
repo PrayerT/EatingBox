106910cfcdeef897105dbcf1c2efed69acb3d839
Prayer <prayer@Prayer-3.local>
2022-10-03 09:06:50 +0800

    temporary backup

--- app/src/main/java/com/example/eatingbox/RecordAdapter.java
+++ app/src/main/java/com/example/eatingbox/RecordAdapter.java
@@ -9,24 +9,47 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import com.example.eatingbox.R;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.TreeSet;
 
-public class MultipleItemsList extends ListActivity {
+public class RecordAdapter extends ListActivity {
 
     private MyCustomAdapter mAdapter;
+    MainActivity mainActivity = new MainActivity();
+    private String name = mainActivity.acc;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAdapter = new MyCustomAdapter();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket = new Socket("123.56.231.80", 4444);
+
+                    OutputStream sendData = socket.getOutputStream();
+                    sendData.write((name + "\r\n").getBytes(StandardCharsets.UTF_8));
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                    String recentRecord = bufferedReader.readLine();
+
+                    sendData.close();
+                    bufferedReader.close();
+                    socket.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         for (int i = 1; i < 50; i++) {
             mAdapter.addItem("item " + i);
-            if (i % 4 == 0) {
-                mAdapter.addSeparatorItem("separator " + i);
-            }
         }
         setListAdapter(mAdapter);
     }
@@ -36,6 +59,7 @@ public class MultipleItemsList extends ListActivity {
         private static final int TYPE_ITEM = 0;
         private static final int TYPE_SEPARATOR = 1;
         private static final int TYPE_MAX_COUNT = TYPE_SEPARATOR + 1;
+        private int itemCount = 0;
 
         private ArrayList mData = new ArrayList();
         private LayoutInflater mInflater;
@@ -43,7 +67,7 @@ public class MultipleItemsList extends ListActivity {
         private TreeSet mSeparatorsSet = new TreeSet();
 
         public MyCustomAdapter() {
-            mInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
         public void addItem(final String item) {
@@ -68,7 +92,7 @@ public class MultipleItemsList extends ListActivity {
 
         @Override
         public String getItem(int position) {
-            return mData.get(position);
+            return mData.get(position).toString();
         }
 
         @Override
@@ -83,13 +107,13 @@ public class MultipleItemsList extends ListActivity {
             System.out.println("getView " + position + " " + convertView + " type = " + type);
             if (convertView == null) {
                 holder = new ViewHolder();
-                convertView = mInflater.inflate(R.layout.item2, null);
-                holder.textView = (TextView)convertView.findViewById(R.id.textSeparator);
+                convertView = mInflater.inflate(R.layout.record_item, null);
+                holder.textView = (TextView) convertView.findViewById(R.id.recordname);
                 convertView.setTag(holder);
             } else {
-                holder = (ViewHolder)convertView.getTag();
+                holder = (ViewHolder) convertView.getTag();
             }
-            holder.textView.setText(mData.get(position));
+            holder.textView.setText(mData.get(position).toString());
             return convertView;
         }
 
